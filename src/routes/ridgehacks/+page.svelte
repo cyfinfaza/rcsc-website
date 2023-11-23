2be20095ba58de6ff1ab9fe197c125942252315f
Brandon Li <sirbrandonthenerd@gmail.com>
2023-11-23 14:37:14 -0500

    update ridgehacks section

--- src/routes/ridgehacks/+page.svelte
+++ src/routes/ridgehacks/+page.svelte
@@ -9,15 +9,16 @@
 	image="/images/ridgehacks_background_graphic.svg"
 />
 
-<HeaderWithImage imageSrc="/images/ridgehacks_background_graphic.svg">
-	The Hackathon
-</HeaderWithImage>
+<HeaderWithImage imageSrc="/images/ridgehacks_background_graphic.svg">RidgeHacks</HeaderWithImage>
 
 <h2>About</h2>
 <p>
-	For those unfamiliar with the term, a hackathon is a collaborative coding event where beginners
-	and professionals alike can employ new technologies to create a final product to pitch in front of
-	judges.
+	A hackathon is an event where students individually or collaboratively create innovative solutions
+	with technology, often using hardware, software, or both! Students present and share their ideas
+	with other participants and can be awarded with prizes for their work. In addition, due to many
+	workshops and experienced mentors, a hackathon is a great learning opportunity and experience.
+	Overall, hackathons are an excellent event for any student with an interest in computer science
+	and programming to attend, no matter the skill level.
 </p>
 <p>
 	Ridgehacks is a <strong>free</strong> 12 hour hackathon running from 8AM to 8PM with judging from industry,
@@ -29,10 +30,63 @@
 </p>
 <p>
 	For any questions or sponsorship inquiries please contact:
-	<a href="mailto:team@ridgehacks.us">team@ridgehacks.us</a>
+	<a href="mailto:ridgecompsci@ridgehacks.us">ridgecompsci@ridgehacks.us</a>
 </p>
+<h2>Details</h2>
+<ul class="margin-list">
+	<li>
+		<strong>Date:</strong> April 6th, 2024
+	</li>
+	<li>
+		<strong>Who can Attend:</strong> Any high school (grades 9-12) or college student who's down to make
+		something awesome. Or anything tech/programming related (please reach out if you are an elementary/middle
+		school student interested in attending).
+	</li>
+	<li>
+		<strong>Cost to Attend:</strong> Free!
+	</li>
+	<li>
+		<strong>Location:</strong> Ridge High School (268 S Finley Ave, Basking Ridge, NJ 07920)
+	</li>
+</ul>
+<h2>FAQ</h2>
+<ul class="margin-qna-list">
+	<li><strong>Q: How can I register?</strong></li>
+	<li>
+		<strong>A:</strong> Stay posted for upcoming updates! The registration form will be posted to this
+		website soon.
+	</li>
+	<li><strong>Q: How will teams work?</strong></li>
+	<li>
+		<strong>A:</strong> You can work in teams of 2 to 4. If you don't have a team, don't worry! You'll
+		be able to form a team at RidgeHacks with other people looking for teams or teams that want more
+		members before hacking begins.
+	</li>
+	<li>
+		<strong>Q: What can I make?</strong>
+	</li>
+	<li>
+		<strong>A:</strong> Anything. Software, hardware, middleware, malware, anything that ends with “ware”
+		and starts with you typing lines of code. And even more than that!
+	</li>
+	<li>
+		<strong>Q: What's in it for me?</strong>
+	</li>
+	<li>
+		<strong>A:</strong> The learning experience, not to mention the free food and prizes! You'll have
+		to show up to find out what the prizes are though. And, of course, the clout and prestige of winning
+		Ridgehacks.
+	</li>
+	<li>
+		<strong>Q: What if I don't know how to code?</strong>
+	</li>
+	<li>
+		<strong>A:</strong> No problem! We'll have workshops and mentors to help you get started and learn
+		along the way.
+	</li>
+</ul>
 <h2>Previous years</h2>
-<ul>
+<ul class="margin-list">
 	<li>
 		<a href="https://2022.ridgehacks.us">2022</a>
 	</li>
